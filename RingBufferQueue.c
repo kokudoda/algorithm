a2a099e6aa004bb161ab0f8a4258fba2b886d905
kokudoda <dkokudo@gmail.com>
2025-04-22 20:11:29 +0900

    参照渡しにした。show関数を追加した

--- RingBufferQueue.c
+++ RingBufferQueue.c
@@ -12,8 +12,8 @@ void init(int* front,int* rear){
 //(*rear+1)%NUM==front  ←満杯の判定
 /*例えば、NUM=5で[10,20,30,40,50]のとき、frontは[0]を指し、rearは[4]を指している。
 　ここで、((*rear+1)%NUM)==frontは、(4+1)%5==0となり、満杯であることが分かる*/
-int enqueue(int data,int front,int* rear){
-    if(((*rear+1)%NUM)==front){
+int enqueue(int data,int* front,int* rear){
+    if(((*rear+1)%NUM)==*front){
         return FAILURE;
     }
     queue[*rear]=data;
@@ -21,8 +21,8 @@ int enqueue(int data,int front,int* rear){
     return SUCCESS;
 }
 
-int dequeue(int* data,int* front,int rear){
-    if(*front==rear){
+int dequeue(int* data,int* front,int* rear){
+    if(*front==*rear){
         printf("空です\n");
         return FAILURE;
     }
@@ -31,17 +31,28 @@ int dequeue(int* data,int* front,int rear){
     return SUCCESS;
 }
 
+void show(int front,int rear){
+    int i=front;
+    while(i != rear){
+        printf("%d ",queue[i]);
+        i=(i+1)%NUM;
+    }
+    printf("\n");
+}
+
 int main(){
     int data,front,rear;
 
     init(&front,&rear);
 
     for(int i=1;i<=NUM;i++){
-        if(enqueue(i*10,front,&rear)==SUCCESS){
+        if(enqueue(i*10,&front,&rear)==SUCCESS){
             printf("ENQUEUE : %d\n",i*10);
         }
     }
-    while(dequeue(&data,&front,rear)==SUCCESS){
+    show(front,rear);
+    
+    while(dequeue(&data,&front,&rear)==SUCCESS){
         printf("DEQUEUE : %d\n",data);
     }
     return 0;
