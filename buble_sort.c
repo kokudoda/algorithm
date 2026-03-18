31db8deb4bb868bfd43ed462be2da0016e4e4c1f
Your Name <you@example.com>
2026-03-18 15:49:35 +0900

    バブルソートの修正

--- buble_sort.c
+++ buble_sort.c
@@ -6,12 +6,12 @@ int array[NUM];
 
 void buble_sort()
 {
-    for(int i = 0; i < NUM;i++){
-        for(int j = i+1; j < NUM; j++){
-            if(array[i] > array[j]){
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+    for(int i = 0; i < NUM - 1;i++){
+        for(int j = 0; j < NUM - 1 - i; j++){
+            if(array[j] > array[j+1]){
+                int temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
             }
         }
     }
